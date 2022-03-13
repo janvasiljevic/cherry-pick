e075a5c98ebb8b85d9fbfc91fca6010e9b46599b
Lan <lan.vukusic@gmail.com>
2022-03-13 08:20:29 +0100

    add assitant wallet

--- api-backend/src/user/user.controller.ts
+++ api-backend/src/user/user.controller.ts
@@ -1,27 +1,35 @@
-import { Controller, Get, Param, Request, UseGuards } from '@nestjs/common';
-import { UserTypeGuard } from '../auth/guard/user-types.guard';
-import { UserService } from './user.service';
-import { Roles } from '../common/decorators/roles.decorator';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { RequestWithUAT } from '../common/interfaces/tokens.interface';
+import {Body, Controller, Get, Param, Patch, Request, UseGuards} from '@nestjs/common';
+import {UserTypeGuard} from '../auth/guard/user-types.guard';
+import {UserService} from './user.service';
+import {Roles} from '../common/decorators/roles.decorator';
+import {ApiOperation, ApiTags} from '@nestjs/swagger';
+import {RequestWithUAT} from '../common/interfaces/tokens.interface';
 
 @Controller('user')
 @ApiTags('Users')
 @UseGuards(UserTypeGuard)
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) {
+    }
 
-  @Get('')
-  @ApiOperation({
-    summary: 'Get all bids',
-    description: 'Get all bids created by user, can be called by user or assistant',
-  })
-  getUserBids(@Request() { user }: RequestWithUAT) {
-    return this.userService.getUserBids(user.sub, user.type);
-  }
+    @Get('')
+    @ApiOperation({
+        summary: 'Get all bids',
+        description: 'Get all bids created by user, can be called by user or assistant',
+    })
+    getUserBids(@Request() {user}: RequestWithUAT) {
+        return this.userService.getUserBids(user.sub, user.type);
+    }
 
-  @Get(':id')
-  getUser(@Param('id') id: string) {
-    return this.userService.getUserById(id);
-  }
+    @Get(':id')
+    getUser(@Param('id') id: string) {
+        return this.userService.getUserById(id);
+    }
+
+    @Roles('ASSISTANT')
+    @Patch('add-wallet-address/:address')
+    updateWalletAddress(@Param('address') address: string, @Request() {user}: RequestWithUAT) {
+        return this.userService.addWalletAddress(address, user.sub);
+    }
+
 }
