29f95a1c77f37a7338dd7ef7c38a28ad008be77c
Lan <lan.vukusic@gmail.com>
2022-03-13 06:29:11 +0100

    test

--- api-backend/src/achievement/achievement.controller.ts
+++ api-backend/src/achievement/achievement.controller.ts
@@ -1,19 +1,34 @@
-import { Body, Controller, Post, Request } from '@nestjs/common';
+import {Body, Controller, Post, Request, UseGuards} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AchievementService } from './achievement.service';
 import { RequestWithUAT } from '../common/interfaces/tokens.interface';
+import {CreateAchievementDto} from "./dto/create-achievement.dto";
+import {UserTypeGuard} from "../auth/guard/user-types.guard";
+import {Roles} from "../common/decorators/roles.decorator";
 
 @Controller('achievement')
+@UseGuards(UserTypeGuard)
 @ApiTags('Achievements')
 export class AchievementController {
   constructor(private readonly achievementService: AchievementService) {}
 
   @Post('')
+  @Roles('ADMIN')
   @ApiOperation({
     summary: 'Create new achievement',
     description: 'Create new smart contract that can mint NFT achievements',
   })
-  createNewAchievement(@Body() createAchievementDto, @Request() { user: admin }: RequestWithUAT) {
+  createNewAchievement(@Body() createAchievementDto: CreateAchievementDto, @Request() { user: admin }: RequestWithUAT) {
     return this.achievementService.create(createAchievementDto, admin.sub);
+  }
+
+  @Post('give-badge')
+  @Roles('ADMIN')
+  @ApiOperation({
+    summary: 'Give a achievement badge',
+    description: 'Create new smart contract that can mint NFT achievements',
+  })
+  giveAchievementBadge(@Body() createAchievementDto: CreateAchievementDto, @Request() { user: admin }: RequestWithUAT) {
+    return this.achievementService.give(createAchievementDto, admin.sub);
   }
 }
--- api-backend/src/achievement/achievement.service.ts
+++ api-backend/src/achievement/achievement.service.ts
@@ -1,29 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../shared/prisma/prisma.service';
-import { CreateAchievementDto } from './dto/create-achievement.dto';
-import { HttpService } from '@nestjs/axios';
-import { Observable } from 'rxjs';
+import {Injectable} from '@nestjs/common';
+import {PrismaService} from '../shared/prisma/prisma.service';
+import {CreateAchievementDto} from './dto/create-achievement.dto';
+import {HttpService} from '@nestjs/axios';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class AchievementService {
-  constructor(private prisma: PrismaService, private httpService: HttpService) {}
+    constructor(private prisma: PrismaService, private httpService: HttpService) {
+    }
 
-  async create(createAchievementDto: CreateAchievementDto, id: string) {
-    const response = await this.httpService
-      .post('http://localhost:3001/create-contract', {
-        name: createAchievementDto.name + '_' + createAchievementDto.level,
-        symbol: createAchievementDto.name + createAchievementDto.level,
-        base_uri: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-      })
-      .toPromise();
+    async create(createAchievementDto: CreateAchievementDto, id: string) {
+        const response = await this.httpService
+            .post('http://172.19.0.3:3001/create-contract', {
+                name: createAchievementDto.name + '_' + createAchievementDto.level,
+                symbol: createAchievementDto.name + createAchievementDto.level,
+                base_uri: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+            })
+            .toPromise();
 
-    await this.prisma.achievement.create({
-      data: {
-        ...createAchievementDto,
-        contractName: createAchievementDto.name + '_' + createAchievementDto.level,
-        contractAddress: response.data.data,
-        createdBy: { connect: { id } },
-      },
-    });
-  }
+        await this.prisma.achievement.create({
+            data: {
+                ...createAchievementDto,
+                contractName: createAchievementDto.name + '_' + createAchievementDto.level,
+                contractAddress: response.data.data,
+                createdBy: {connect: {id}},
+            },
+        });
+    }
+
+    async give(createAchievementDto: CreateAchievementDto, id: string) {
+        const response = await this.httpService
+            .post('http://nft-backend:3001/create-nft', {
+                contract_name: createAchievementDto.name,
+                address: '0xD69e7236d75a8b4705396c78A3b6C3CC155B2667',
+                contract_address: '0xc9f543cA821b8740915F01A9d25e7055870Ecbc8',
+            })
+            .toPromise();
+
+    }
 }
+
+
